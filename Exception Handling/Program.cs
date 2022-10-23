b1b8633e92c5da87fb880100d7f205a87e1ac9c6
NET <NET@DESKTOP-NSMUMFG>
2022-10-23 16:42:52 +0600

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int m = int.Parse("56"), n = 0;
+                int r = m / n;
+                var ex = new FileNotFoundException("Specified file not found.");
+                throw ex;
+
+            }
+            catch (FormatException fex)
+            {
+                Console.WriteLine("Input Error: " + fex.Message);
+            }
+            catch (DivideByZeroException dex)
+            {
+                Console.WriteLine("Illegal Operation: " + dex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unkonwn Error: " + ex.Message);
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }//Main
+    }//Class
+}//Namespace
